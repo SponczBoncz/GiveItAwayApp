37c6a72ef228f9eefa8c66a6c2dd7ecf90370de9
SponczBoncz <bku82@o2.pl>
2024-04-14 15:28:31 +0200

    Added models: Category Institution Donation
    Dinamic statistics work

--- GiveItAwayApp/my_app/models.py
+++ GiveItAwayApp/my_app/models.py
@@ -1,3 +1,59 @@
+from django.conf import settings
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+TYPE_CHOICES = [
+    (0, 'fundacja'),
+    (1, 'organizacja pozarządowa'),
+    (2, 'zbiórka lokalna'),
+]
+
+class Category(models.Model):
+    '''
+    Attributes:
+        name: CharField with max length of 64 characters. Should be unique.
+
+    Methods:
+        str: Returns the name of the category.
+    '''
+
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Institution(models.Model):
+    '''
+    Attributes:
+        name: CharField with max length of 64 characters. Should be unique.
+        description: TextField to provide additional information about the institution.
+        type: IntegerField with choices defined in TYPE_CHOICES (fundacja, organizacja pozarządowa, zbiórka lokalna).
+                Default value is 0 (fundacja).
+        categories: Many-to-Many relationship with Category model.
+
+
+    Methods:
+        str: Returns the name of the category.
+    '''
+
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE_CHOICES, default=0)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+class Donation(models.Model):
+
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=64)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
--- GiveItAwayApp/my_app/views.py
+++ GiveItAwayApp/my_app/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.db.models import Count, Sum
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from my_app.models import Donation
 
 
 class LandingPageView(View):
     template_name = 'my_app/index.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        total_bags_quantity = Donation.objects.aggregate(Sum('quantity'))
+        all_donated_institutions = len(Donation.objects.values('institution').annotate(count=Count('institution')))
+        context = {
+            'total_bags_quantity': total_bags_quantity,
+            'all_donated_institutions': all_donated_institutions,
+        }
+        return render(request, self.template_name, context)
 
 class LoginView(View):
     template_name = 'my_app/login.html'
